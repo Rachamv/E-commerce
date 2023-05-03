4c7d6b5ede4ab94494f489574bd207feec2f7277
Rachamv <rkutalian@gmail.com>
2023-05-03 21:17:22 +0100

    todays work

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from core.views import index, category_list_view, product_list_view, category_product_list__view
+from core.views import index, category_list_view, product_list_view, category_product_list__view, vendor_list_view
 
 
-# add_to_cart, add_to_wishlist, ajax_add_review, ajax_contact_form, cart_view, , checkout_view, customer_dashboard, delete_item_from_cart, filter_product, index, make_address_default, order_detail, payment_completed_view, payment_failed_view, product_detail_view,  remove_wishlist, search_view, tag_list, update_cart, vendor_detail_view, vendor_list_view, wishlist_view, contact, about_us, purchase_guide, privacy_policy, terms_of_service
+# add_to_cart, add_to_wishlist, ajax_add_review, ajax_contact_form, cart_view, , checkout_view, customer_dashboard, delete_item_from_cart, filter_product, index, make_address_default, order_detail, payment_completed_view, payment_failed_view, product_detail_view,  remove_wishlist, search_view, tag_list, update_cart, vendor_detail_view,  wishlist_view, contact, about_us, purchase_guide, privacy_policy, terms_of_service
 
 app_name = "core"
 
@@ -15,4 +15,8 @@ urlpatterns = [
     # Category
     path("category/", category_list_view, name="category-list"),
     path("category/<cid>/", category_product_list__view, name="category-product-list"),
+
+    # Vendor
+    path("vendor/", vendor_list_view, name="vendor-list"),
+    path("vendor/<vid>/", vendor_detail_view, name="vendor-detail"),
 ]
--- core/views.py
+++ core/views.py
@@ -48,3 +48,10 @@ def category_product_list__view(request, cid):
         "products":products,
     }
     return render(request, "core/category-product-list.html", context)
+
+def vendor_list_view(request):
+    vendors = Vendor.objects.all()
+    context = {
+        "vendors": vendors,
+    }
+    return render(request, "core/vendor-list.html", context)
